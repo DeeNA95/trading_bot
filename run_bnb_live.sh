e743cfe0d617b2d9b8520022f1f2fc982d270b01
DerekAmuna <derek@reallygreattech.com>
2025-03-08 10:30:30 +0000

    update model paths

--- run_bnb_live.sh
+++ run_bnb_live.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 pipenv run python inference.py \
-  --model_path gs://crypto_trading_models/LSTM/ETH/best_model.pt \
+  --model_path gs://crypto_trading_models/LSTM/BNB/best_model.pt \
   --model_type lstm \
-  --symbol ETHUSDT \
+  --symbol BNBUSDT \
   --window_size 24 \
   --leverage 20 \
   --interval 15m \
   --initial_balance 12 \
   --stop_loss 0.05 \
-  --risk_reward 1.1 \
+  --risk_reward 1.5 \
   --sleep_time 60
